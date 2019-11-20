c3eee56133d4fb916631e5cb202433a589c16daf
Kevin Meyer <kpm216@gmail.com>
2019-11-20 01:22:59 -0500

    Bare bones project

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const config = require('./config.json')
+
+const accountSid = config['accountSid']
+const authToken = config['authToken']
+const client = require('twilio')(accountSid, authToken);
+
+const PORT = process.env.PORT || 3000
+
+app.post('/sms', function(req, res) {
+    console.log('---------------MESSAGE RECEIVED-----------------')
+})
+
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+})
